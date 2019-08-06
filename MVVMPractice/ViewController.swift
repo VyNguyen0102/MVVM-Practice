a6329ff46e90611ccd5ef6aac4bdcbd94d830a09
Vy Nguyen <vy.nv@sutrixsolutions.com>
2019-08-06 11:47:22 +0700

    init project

---
+++ MVVMPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVMPractice
+//
+//  Created by iOS Dev on 8/6/19.
+//  Copyright © 2019 iOS Dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
